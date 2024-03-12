4ab271b609df61db5366d6c9c5bff2f49b48e80f
QuocDai1997 <162120667+QuocDai1997@users.noreply.github.com>
2024-03-12 22:57:22 +0700

    Add files via upload

---
+++ Book/src/ProgrammingBook.java
@@ -0,0 +1,47 @@
+public class ProgrammingBook extends Book {
+    private String language;
+    private String framework;
+
+    public ProgrammingBook() {
+    }
+
+    public ProgrammingBook(String language) {
+        this.language = language;
+    }
+
+    public ProgrammingBook(String code, String name, int price, String author) {
+        super(code, name, price, author);
+    }
+
+    @Override
+    public double getAMount(){
+        return getPrice()*0.9;
+    }
+
+    public ProgrammingBook(String code, String name, int price, String author, String language) {
+        super(code, name, price, author);
+        this.language = language;
+    }
+
+    public ProgrammingBook(String code, String name, int price, String author, String language, String framework) {
+        super(code, name, price, author);
+        this.language = language;
+        this.framework = framework;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getFramework() {
+        return framework;
+    }
+
+    public void setFramework(String framework) {
+        this.framework = framework;
+    }
+}
